cec5803b8464ce0f8c7947e47899ce9a34fabf5a
Madalin Popa <contact@madalinpopa.com>
2026-01-14 21:55:54 +0200

    refactor: move health check handler

---
+++ internal/interfaces/web/handler/healthcheck.go
@@ -0,0 +1,13 @@
+package handler
+
+import (
+	"net/http"
+)
+
+func CheckHealthHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	_, err := w.Write([]byte("OK"))
+	if err != nil {
+		return
+	}
+}
