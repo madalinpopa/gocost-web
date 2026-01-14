f4d800b5f1daea7ecbb324242b01998db34d1705
Madalin Popa <contact@madalinpopa.com>
2026-01-14 22:22:08 +0200

    refactor: response package

---
+++ internal/interfaces/web/response.go
@@ -0,0 +1,25 @@
+package web
+
+import (
+	"log/slog"
+)
+
+type Response struct {
+	logger *slog.Logger
+	Handle ErrorHandler
+	Htmx   HtmxHandler
+	Notify NotifyHandler
+}
+
+func NewResponse(logger *slog.Logger) Response {
+
+	errHandler := newErrorHandlers(logger)
+	notifier := newNotify(logger)
+	htmxRes := newHtmx(logger, errHandler)
+	return Response{
+		logger: logger,
+		Notify: notifier,
+		Handle: errHandler,
+		Htmx:   htmxRes,
+	}
+}
