e9b89c26b5eb78d750a06e75378c20b789bad5c3
Madalin Popa <contact@madalinpopa.com>
2026-01-24 20:29:50 +0200

    feat(web/respond): add HTMX response helpers

---
+++ internal/interfaces/web/respond/htmx.go
@@ -0,0 +1,43 @@
+package respond
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type htmx struct {
+	errHandler ErrorHandler
+}
+
+func NewHtmx(h ErrorHandler) HtmxHandler {
+	return htmx{
+		errHandler: h,
+	}
+}
+
+func (h htmx) Redirect(w http.ResponseWriter, url string) {
+	w.Header().Set("HX-Redirect", url)
+	w.WriteHeader(http.StatusFound)
+}
+
+func (h htmx) Location(w http.ResponseWriter, r *http.Request, url string, target string, swap string) {
+	locationObj := map[string]string{
+		"path": url,
+	}
+
+	if target != "" {
+		locationObj["target"] = target
+	}
+
+	if swap != "" {
+		locationObj["swap"] = swap
+	}
+
+	locationJSON, err := json.Marshal(locationObj)
+	if err != nil {
+		h.errHandler.ServerError(w, r, err)
+		return
+	}
+	w.Header().Set("HX-Location", string(locationJSON))
+	w.WriteHeader(http.StatusOK)
+}
