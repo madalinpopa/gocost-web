34a8ae721294a315c985176cc7a1b53b740ded90
Madalin Popa <contact@madalinpopa.com>
2026-01-30 19:58:31 +0200

    test(identity): Fix indentation and password value in TestNewUser

--- internal/domain/identity/entity_test.go
+++ internal/domain/identity/entity_test.go
@@ -12,20 +12,19 @@ func TestNewUser(t *testing.T) {
 		// Arrange
 		id, _ := identifier.NewID()
 		username, _ := NewUsernameVO("testuser")
-				email, _ := NewEmailVO("test@example.com")
-				password, _ := NewPasswordVO("$2a		2$abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12")
-				currency, _ := NewCurrencyVO("USD")
-		
-				// Act
-				user := NewUser(id, username, email, password, currency)
-		
-				// Assert
-				assert.NotNil(t, user)
-				assert.Equal(t, id, user.ID)
-				assert.Equal(t, username, user.Username)
-				assert.Equal(t, email, user.Email)
-				assert.Equal(t, password, user.Password)
-				assert.Equal(t, currency, user.Currency)
-			})
-		}
-		
+		email, _ := NewEmailVO("test@example.com")
+		password, _ := NewPasswordVO("$2a2$abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ12")
+		currency, _ := NewCurrencyVO("USD")
+
+		// Act
+		user := NewUser(id, username, email, password, currency)
+
+		// Assert
+		assert.NotNil(t, user)
+		assert.Equal(t, id, user.ID)
+		assert.Equal(t, username, user.Username)
+		assert.Equal(t, email, user.Email)
+		assert.Equal(t, password, user.Password)
+		assert.Equal(t, currency, user.Currency)
+	})
+}
